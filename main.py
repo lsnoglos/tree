2d93f12f47e40a6d320e3d62df2d67b101c0897c
lsnoglos <lsnoglos@gmail.com>
2024-04-30 23:23:00 -0600

    add: show window with list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import pyodbc
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 
 load_dotenv()
 
@@ -10,17 +12,40 @@ DB_USER=os.getenv('DB_USER')
 DB_PASSWORD=os.getenv('DB_PASSWORD')
 DRIVER='ODBC Driver 17 for SQL Server'
 
-def create_connection (driver, server, db, user, password):
+def create_connection ():
     try:
-        connection_string = f'DRIVER={{{driver}}};SERVER={server};DATABASE={db};UID={user};PWD={password}'
-        conn = pyodbc.connect(connection_string)
-        print("Conexio'n exitosa.")
-        return conn
+        connection_string = f'DRIVER={{{DRIVER}}};SERVER={DB_HOST};DATABASE={DB_NAME};UID={DB_USER};PWD={DB_PASSWORD}'
+        print("Conexión exitosa.")
+        return pyodbc.connect(connection_string)
     except Exception as e:
         print("Error de conexio'n", e)
         return None
 
-connection = create_connection(DRIVER, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD)
+def fetch_families():
+    conn = create_connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT family_id, family_name FROM Families")
+            return cursor.fetchall()
+        finally:
+            conn.close()
+    else:
+        return []
 
-if(connection):
-    connection.close()
+def main():
+    root = tk.Tk()
+    root.title("Árbol genealógico")
+    root.geometry("400x300")
+
+    family_listBox = tk.Listbox(root)
+    family_listBox.pack(pady = 20)
+    families = fetch_families()
+
+    for family in families:
+        family_listBox.insert(tk.END, family[1])
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
